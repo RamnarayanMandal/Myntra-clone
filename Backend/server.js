e4ee1d432ca39e92ddf43680e38c36839b948df5
anish0905 <anishanish0102@gmail.com>
2024-06-30 22:19:07 +0530

    database connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+
+const cors = require("cors");
+
+
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+// Home route for admin routes
+app.get("/", (req, res) => {
+  res.send("API is running successfully");
+});
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
